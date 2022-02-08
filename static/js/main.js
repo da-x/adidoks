ef6d9173f830a25258bce170668eeb9abb0b0be0
Dan Aloni <dan@kernelim.com>
2022-02-08 21:34:18 +0200

    Highlight luminosity toggle

--- static/js/main.js
+++ static/js/main.js
@@ -1,16 +1,26 @@
 // Set darkmode
 document.getElementById('mode').addEventListener('click', () => {
-
-    document.body.classList.toggle('dark');
-    localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
-  
+  document.body.classList.toggle('dark');
+  localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
 });
-  
-// enforce local storage setting but also fallback to user-agent preferences
-if (localStorage.getItem('theme') === 'dark' || (!localStorage.getItem('theme') && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
-  
-  document.body.classList.add('dark');
-  
-}
+
+document.getElementById('code-mode').addEventListener('click', () => {
+    document.body.classList.toggle('code-theme-inverse');
+    localStorage.setItem('code-theme',
+        document.body.classList.contains('code-theme-inverse')
+          ? 'enabled' : 'disabled');
+});
 
 document.body.classList.add('darkness-determined');
+
+// enforce local storage setting but also fallback to user-agent preferences
+if (localStorage.getItem('theme') === 'dark' ||
+  (!localStorage.getItem('theme') && window.matchMedia("(prefers-color-scheme: dark)").matches))
+{
+  document.body.classList.add('dark');
+}
+
+if (localStorage.getItem('code-theme') === 'enabled')
+{
+  document.body.classList.add('code-theme-inverse');
+}
